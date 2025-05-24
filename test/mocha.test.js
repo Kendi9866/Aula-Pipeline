4d968f7195e444ca61c372b235e58182295db1e1
kendas <kendigabriel29@gmail.com>
2025-05-24 09:07:51 -0300

    sera que ela é do job

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,1 +1,7 @@
-mo
+const assert = require('assert')
+
+describe('Exemplo de teste', function(){
+    it('deve somar dois números corretamente', function(){
+        assert.strictEqual(1 + 1, 2);
+    })
+})
